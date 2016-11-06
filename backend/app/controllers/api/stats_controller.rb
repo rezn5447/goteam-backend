6980e0d2333aa626234d330ebfdb7d66d9a84d29
lokpio <lokpio322@gmail.com>
2016-11-05 19:00:09 -0700

    Add the stat create method

--- backend/app/controllers/api/stats_controller.rb
+++ backend/app/controllers/api/stats_controller.rb
@@ -1,8 +1,19 @@
-class StatsController < ApplicationController
-	before_action :user_exits?(cookies[:token],params[:user_id])
-
+class Api::StatsController < ApplicationController
 
 	def create
-		user = User.find(params[:user_id])
+		respond_to do |format|
+			p params
+			user = user_exist?("jgbCP2MuzW5yAPkGGWEmzQ",params[:user_id])
+			stat = Stat.new(user_id:params[:user_id],sport_id: params[:stat][:sport_id], active: true)
+			if user && stat.save
+				format.html {render json: stat}
+				format.json {render json: stat}
+			else
+				format.html {render json: stat.errors.full_messages}
+				format.json {render json: stat.errors.full_messages}
+			end		
+		end
+		
 	end
+
 end
