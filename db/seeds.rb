2370d0736c587cfdb86137064768b0cbdd7cb401
lokpio <lokpio322@gmail.com>
2016-11-06 19:28:32 -0800

    Change the seed file to add another stats for a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,6 +74,7 @@ Stat.create(active: true , user_id:1, sport_id:1, win:1 )
 Stat.create(active: true , user_id:2, sport_id:1, win:2, rating:45 )
 Stat.create(active: true , user_id:3, sport_id:2, win:3 )
 Stat.create(active: true , user_id:4, sport_id:2, win:4 )
+Stat.create(active: true , user_id:1, sport_id:2, win:1 )
 
 
 
