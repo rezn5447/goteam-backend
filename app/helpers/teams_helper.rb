8098a89913d4ce0e3efa5f7328cc9cee21625871
King Heng Jonathan Lok <email": null>
2016-11-07 10:08:06 -0800

    Add the assign season method in team model

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,20 +1,15 @@
 module TeamsHelper
-	def assign_players(players,total)
+	def assign_players(players,total,sport_id)
 		players = players.order(:updated_at)
 		i = 0
 		teams = []
 		while i < total
 			teams << players[i]
 			i+=1
-		end 
-		teams = assign_rand_teams(teams,total)
-	end
-
-	private
-
-	def assign_rand_teams(teams,total)
-		teams= teams.shuffle
-		team_num = total/2
-		{Home:teams[0...team_num],Away:teams[team_num..-1]} 
+		end
+		teams = Team.shuffle_rand_teams(teams,total,sport_id)
+		# match = create_match
+		home = Team.create_teams(teams[:Home],true)
+		away = Team.create_teams(teams[:Away],false)
 	end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,35 @@ class Team < ApplicationRecord
   has_many :userteams
 
   validates :season, :home, presence: true
+
+  def self.shuffle_rand_teams(teams,total,sport_id)
+    teams= teams.shuffle
+    team_num = total/2
+    {Home:teams[0...team_num],Away:teams[team_num..-1]}
+  end
+
+  def self.create_teams(players, home)
+    season = season?
+    team = Team.new(sport_id:sport_id,Home:home,season:season)
+    if team.save
+
+    else
+    end
+  end
+
+  private
+
+  def self.season?
+    month =  Time.now.strftime("%m").to_i
+    year =  Time.now.strftime("%Y")
+    if month == 12 || month <=2
+      "Winter " + year
+    elsif month > 2 && month <=5
+      "Spring " + year
+    elsif month > 5 && month <= 8
+      "Summer " + year
+    else
+      "Fall " + year
+    end
+  end
 end
