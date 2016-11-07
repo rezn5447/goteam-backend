36b81c72eaa27f80e08798b72ff2ebf21bf65b95
lokpio <lokpio322@gmail.com>
2016-11-06 20:57:28 -0800

    Add the sports pass params

--- app/helpers/sports_helper.rb
+++ app/helpers/sports_helper.rb
@@ -2,7 +2,7 @@ module SportsHelper
   def pass_sport_params(sports)
     config = []
     sports.each do |sport|
-      in_config = {id: sport.id, name: sport.name,total_players: sport.total_players}
+      in_config = {id: sport.id, name: sport.name,total_players: sport.total_players, category:sport.category}
       config << in_config
     end
     config
