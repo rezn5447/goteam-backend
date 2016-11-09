61fefe6ba4d2f21efdbc39dc780770b59632de76
King Heng Jonathan Lok <email": null>
2016-11-09 14:35:51 -0800

    Trail

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::SessionsController < ApplicationController
 
   def create
     respond_to do |format|
-      user = User.authenticate(user_params[:email],user_params[:password])
+      user = User.authenticate(params[:email],params[:password])
       if user
         format.html {render json: {user:pass_user_params(user),token: gen_token(user)}}
         format.js {render json: {user:pass_user_params(user),token: gen_token(user)}}
@@ -13,12 +13,6 @@ class Api::SessionsController < ApplicationController
         format.json {render json: {message:"User does not exist"}}
       end
     end
-  end
-
-  private
-
-  def user_params
-    params.require(:session).permit(:email,:password)
   end
 
 end
