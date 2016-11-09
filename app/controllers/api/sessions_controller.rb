ff3ec119cfba575d87c8593495a26dc2ef39757b
King Heng Jonathan Lok <email": null>
2016-11-09 14:52:44 -0800

    Fix the data structure of session create

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,9 +5,9 @@ class Api::SessionsController < ApplicationController
       user = User.authenticate(params[:email],params[:password])
       if user
         p pass_user_params(user)
-        format.html {render json: {user:pass_user_params(user),token: gen_token(user)}}
-        format.js {render json: {user:pass_user_params(user),token: gen_token(user)}}
-        format.json {render json: {user:pass_user_params(user),token: gen_token(user)}}
+        format.html {render json: [{user:pass_user_params(user)},{token: gen_token(user)}]}
+        format.js {render json: [{user:pass_user_params(user)},{token: gen_token(user)}]}
+        format.json {render json: [{user:pass_user_params(user)},{token: gen_token(user)}]}
       else
         format.html {render json: {message: "User does not exist"}}
         format.js {render json: {message: "User does not exist"}}
