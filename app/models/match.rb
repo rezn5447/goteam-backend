5570e88dea90147dbce77d2cfef572c616f4f78c
King Heng Jonathan Lok <email": null>
2016-11-10 14:00:07 -0800

    fix the time of createing team

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,7 +7,7 @@ class Match < ApplicationRecord
   def self.create_match(sport_id,location)
     time = ["09:00","12:00","15:00","18:00"].sample
     weekend = [:sunday,:saturday].sample
-    date = Time.now.next_week(weekend).strftime("%d/%m/%Y #{time}")
+    date = Time.now.next_week(weekend).strftime("%Y-%m-%d #{time}")
     match = Match.new(location:location,sport_id:sport_id, date:date)
   end
 end
